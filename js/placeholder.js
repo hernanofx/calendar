6a5b1d78eaa095addacdf4fe8e4f9ac0913168a4
hernanofx <hernanofx@hotmail.com>
2016-09-30 19:55:59 -0300

    edditing inputs, styles, divs

---
+++ js/placeholder.js
@@ -0,0 +1,14 @@
+// Remueve el selector de fechas hasta que uno clickea en el
+
+$('input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="month"], input[type="time"], input[type="week"]').each(function() {
+    var el = this,
+        type = $(el).attr('type');
+    if ($(el).val() == '') $(el).attr('type', 'text');
+    $(el).focus(function() {
+        $(el).attr('type', type);
+        el.click();
+    });
+    $(el).blur(function() {
+        if ($(el).val() == '') $(el).attr('type', 'text');
+    });
+});
